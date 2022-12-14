e4d945f8cc5ef4e3ff0ce95c68b6fadd1aa82e01
DanilFrontend <danilstorage@gmail.com>
2022-12-15 02:52:16 +0500

    forwarding the code to typescript

--- db.ts
+++ db.ts
@@ -1,13 +1,13 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    `${process.env.DB_PASSWORD}`,
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
         port: process.env.DB_PORT
     }
-
 )
+export default sequelize;
--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
+
+import sequelize from "./db";
 require('dotenv').config()
 const express = require('express')
 const PORT = process.env.PORT || 5000
-const Sequelize = require('./db')
+
 const app = express()
 
 const start = async () => {
     try{
-        await Sequelize.authenticate()
-        await Sequelize.sync()
+        await sequelize.authenticate()
+        await sequelize.sync()
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
     }catch (e){
         console.log(e)
     }
+
 }
+start()
