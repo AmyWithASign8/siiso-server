a53f42f6dcc529db2cba8663665792227877141b
DanilFrontend <danilstorage@gmail.com>
2022-12-24 23:59:05 +0500

    get all comments function and tryed create comment

--- controllers/commentController.ts
+++ controllers/commentController.ts
@@ -1,38 +1,42 @@
-import {ApiErrors} from "../error/ApiError";
+import { ApiErrors } from "../error/ApiError";
 
-const {Comments} = require('../models/models')
+const { Comments, User } = require("../models/models");
 class CommentController {
-    async create(req: any, res: any, next: any) {
-        try{
-            const {comment, newsId, userId} = req.body
-            const comments = Comments.create({comment, newsId, userId})
-            return res.json(comments)
-        }catch (e){
-            next(ApiErrors)
-        }
+  async create(req: any, res: any, next: any) {
+    try {
+      const newsId = req.params;
+      const { comment, userId } = req.body;
+      const comments = Comments.create({ comment, userId, newsId });
+      return res.json(comments);
+    } catch (e) {
+      next(ApiErrors);
     }
-    async getAll(req: any, res: any) {
-        const {id} = req.params
-        let comment;
-            comment = await Comments.findAll(
-                {where: {newsId: id}},
-            )
-        return res.json(comment)
+  }
+  async getAll(req: any, res: any) {
+    const { id } = req.params;
+    let comment;
+    comment = await Comments.findAll({
+      where: { newsId: id },
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    return res.json(comment);
+  }
+  async delete(req: any, res: any, next: any) {
+    const { id } = req.params;
+    try {
+      const comments = await Comments.destroy({
+        where: {
+          newsId: id,
+        },
+      });
+      return res.json("Комментарий успешно удален");
+    } catch (e) {
+      next(ApiErrors);
     }
-    async delete(req: any, res: any, next: any) {
-        const {id} = req.params
-        try{
-            const comments = await Comments.destroy(
-                {
-                    where: {
-                        newsId: id
-                    }
-                }
-            )
-            return res.json('Комментарий успешно удален')
-        }catch (e){
-            next(ApiErrors)
-        }
-    }
+  }
 }
-module.exports = new CommentController()
+module.exports = new CommentController();
--- routes/commentRouter.ts
+++ routes/commentRouter.ts
@@ -1,9 +1,9 @@
-const Express = require('express');
-const routes = Express.Router()
-const commentController = require('../controllers/commentController')
+const Express = require("express");
+const routes = Express.Router();
+const commentController = require("../controllers/commentController");
 
-routes.post('/',commentController.create)
-routes.get('/getallcomments/:id',commentController.getAll)
-routes.delete('/:id',commentController.delete)
+routes.post("/createcomment/:id", commentController.create);
+routes.get("/getallcomments/:id", commentController.getAll);
+routes.delete("/:id", commentController.delete);
 
-module.exports = routes
+module.exports = routes;
